bb5b4a952515417817b8ba162fb549ccfe3ba5a1
ahormozfar <a.hormozfar@yahoo.de>
2024-07-12 12:50:20 +0200

    01 / 1. Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,12 +2,18 @@ package com.cc.java;
 
 public class App {
     
+
     public static void main(String[] args) {
 
+        // Instanziierung --> Erzeugen eines Objektes
+        // System.out.println(new Cat());
+        
+        // cat --> Referenzvariable
+        // Cat --> Referenztyp (Klasse)
+        Cat cat = new Cat();
 
+        System.out.println(cat);
     }
-
-   
 
 }
 
