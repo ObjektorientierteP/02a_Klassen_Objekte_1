721023084d4a366b43952923e30bdf065a8796ca
ahormozfar <a.hormozfar@yahoo.de>
2024-07-15 11:47:11 +0200

    implizite Typumwandlung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -10,21 +10,37 @@ public class App {
         
         // cat --> Referenzvariable
         // Cat --> Referenztyp (Klasse)
-        Cat cat = new Cat();
-        System.out.println("Blick von außen: " + cat);
-        cat.tellYoutAddress();
-
-        System.out.println("---------------------");
-
-        Cat cat2 = new Cat();
-        System.out.println("Blick von außen: " + cat2);
-
+        // Cat cat = new Cat();
+        // System.out.println("Blick von außen: " + cat);
         // cat.tellYoutAddress();
 
-        cat2.tellYoutAddress();
+        // App app = new App();
+        // app.output("test");
 
+        // oder bei Methode "static" verwenden
+        // output("test");
+
+        Cat cat = new Cat();
+        output("Blick von Innen: " + cat.getInstanceVariable());
+
+        //System.out.println("---------------------");
+
+        //Cat cat2 = new Cat();
+        //System.out.println("Blick von außen: " + cat2);
+
+        //cat.tellYoutAddress();
+
+        //cat2.tellYoutAddress();
 
     }
+
+    // Statische Methode, die in der Klasse ausgeführt wird ...
+    public static void output(String  cat){
+        System.out.println(cat);
+ 
+    }
+
+
 
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,9 +2,17 @@ package com.cc.java;
 
 public class Cat {
     
-    void tellYoutAddress(){
-        // System.out.println("hi");
-        System.out.println("Blick von innen: " + this);
+    // void tellYoutAddress(){
+    //     // System.out.println("hi");
+    //     //System.out.println("Blick von innen: " + this);
+
+    //     App.output("Blick von innen: " + this);
+    // }
+
+    public Cat getInstanceVariable(){
+
+        return this;
+
     }
 
 }
